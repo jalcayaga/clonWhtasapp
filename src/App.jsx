caf9d5d5e4927abd9d8b513cb6c888046694146b
jalcayaga <javier.alcayaga@subred.cl>
2023-07-04 15:10:01 -0500

    vite + conexion a supabase

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import Messages from './components/Messages'
+import './App.css'
+
+function App() {
+ 
+  return (
+   
+      <div className='App'>
+
+        <h1>Whatsapp Clone</h1>
+        <br />
+    <Messages/>
+      </div>
+   
+   
+  )
+}
+
+export default App
---
+++ src/components/Message.jsx
@@ -0,0 +1,12 @@
+
+const Message = ({ message, date, email }) => {
+  return (
+    <div className="card">
+      <p>{message}</p>
+      <span>{date}</span>
+      <span className="user-email">{email}</span>
+    </div>
+  );
+};
+
+export default Message;
